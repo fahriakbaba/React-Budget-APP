fea5aad6b36e50d87e999e891ebe1e21c549f337
Fahri Akbaba <fahriakbaba@gmail.com>
2023-04-16 21:34:19 +0300

    added item to array

--- src/components/MainContent/Main.js
+++ src/components/MainContent/Main.js
@@ -3,13 +3,19 @@ import styles from "./Main.module.css";
 import data from "../../data.json";
 
 function Main() {
+  const [cart, setCart] = React.useState([]);
+  console.log("cart: ", cart);
+
+  const addToCart = (obj) => {
+    setCart(prevState => ([...prevState, obj]));
+  }
 
   const dataMap = data.shoppingList.map(item => (
     <section key={item.id} className={styles.itemCard}>
       <img className={styles.image} src={item.url} alt="shopping_photo" />
       <h4 className={styles.title}>{item.name}</h4>
       <p className={styles.price}>{item.price} $</p>
-      <button className={styles.btn}>Add to cart</button>
+      <button className={styles.btn} onClick={() =>addToCart(item)}>Add to cart</button>
     </section>
   ));
 
