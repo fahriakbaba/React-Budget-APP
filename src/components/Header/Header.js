e67c362a01ec6dd0db6997924c08e3fff4fa2e91
Fahri Akbaba <fahriakbaba@gmail.com>
2023-04-16 19:36:53 +0300

    added icon to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { GrMoney } from 'react-icons/gr';
 
 
 function Header() {
     return (
         <header className={styles.header}>
-            <h2 className={styles.title}>Money Spending APP</h2>
+            <h2 className={styles.title}><GrMoney className={styles.icon} />Money Spending APP</h2>
         </header>
     );
 }
